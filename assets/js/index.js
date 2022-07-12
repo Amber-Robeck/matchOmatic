510a4846fae8eed94332b02f14b25fe2129e8183
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-12 10:23:44 -0500

    Dynamically adding grid and img card to html, minor stylings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,8 @@
+//random pictures that will be api call
 const dData = [
     {
         name: "gZilla",
-        img: "./assets/images/gzilla.png",
+        img: "./assets/images/gZilla.png",
     },
     {
         name: "mike",
@@ -9,7 +10,7 @@ const dData = [
     },
     {
         name: "gZilla",
-        img: "./assets/images/gzilla.png",
+        img: "./assets/images/gZilla.png",
     },
     {
         name: "mike",
@@ -17,6 +18,27 @@ const dData = [
     }
 ];
 
-
+//shuffle the array
 dData.sort(() => Math.random() - 0.5);
 console.log(dData);
+
+let grid = document.createElement("div");
+// const grid = document.querySelector("#grid");
+
+function makeGame() {
+    //create the grid
+    let grid = document.createElement("div");
+    grid.setAttribute("id", "grid");
+    document.body.prepend(grid);
+    //create the cards
+    for (let i = 0; i < dData.length; i++) {
+        let card = document.createElement("img");
+        card.setAttribute("class", "card");
+        card.setAttribute("data-name", dData[i].name);
+        card.setAttribute("data-id", i);
+        card.setAttribute("src", dData[i].img);
+        grid.appendChild(card);
+    }
+}
+
+makeGame();
