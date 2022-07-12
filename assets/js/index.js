9b99fb80003572eac12d4e3e9a7abf8590f865af
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-12 11:06:23 -0500

    Cards change back to background image if no match is found

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,19 +56,31 @@ function turnCard() {
     this.setAttribute("src", dData[cardId].img);
     if (userChoice.length < 2) {
         // console.log(dData[cardId].name);
-        userChoice.push(dData[cardId].name);
+        //had to add object push to change background image back to background.png
+        userChoice.push({ id: cardId, name: dData[cardId].name });
         console.log(userChoice);
     }
     //if I use else if, this condition doesn't run until there are three cards Perhaps change to switch for long term
     //create checkMatch function to store these items
     if (userChoice.length === 2) {
         console.log("checking for match");
-        if (userChoice[0] === userChoice[1]) {
+        if (userChoice[0].name === userChoice[1].name) {
             console.log("match");
             userChoice = [];
         }
         else {
             console.log("no match");
+            //change cards back to background image
+            // userChoice[0].id.setAttribute("src", "./assets/images/background.png");
+            // userChoice[1].id.setAttribute("src", "./assets/images/background.png");
+            // cardId.setAttribute("src", "./assets/images/background.png");
+            // this.setAttribute("src", "./assets/images/background.png");
+            console.log(userChoice[0].id, userChoice[1].id);
+            let choice1 = document.querySelector('[data-id="' + userChoice[0].id + '"]');
+            choice1.setAttribute("src", "./assets/images/background.png");
+            let choice2 = document.querySelector('[data-id="' + userChoice[1].id + '"]');
+            choice2.setAttribute("src", "./assets/images/background.png");
+            // console.log(choice1);
             userChoice = [];
         }
     }
