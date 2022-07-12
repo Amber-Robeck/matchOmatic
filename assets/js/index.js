4af5ca059db753d8119ea98685254505b19886e6
Amber-Robeck <94136164+Amber-Robeck@users.noreply.github.com>
2022-07-12 09:57:24 -0500

    Added dData with images and added a sort to the array to randomize order to be displayed

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,22 @@
-console.log("connected");
+const dData = [
+    {
+        name: "gZilla",
+        img: "./assets/images/gzilla.png",
+    },
+    {
+        name: "mike",
+        img: "./assets/images/mike.png",
+    },
+    {
+        name: "gZilla",
+        img: "./assets/images/gzilla.png",
+    },
+    {
+        name: "mike",
+        img: "./assets/images/mike.png",
+    }
+];
+
+
+dData.sort(() => Math.random() - 0.5);
+console.log(dData);
